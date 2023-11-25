186ee42aa82d3cd3cb6484624dee1caf65a0d4f3
dhlee <jsleeg7279@naver.com>
2023-11-25 13:03:13 +0000

    Update test

--- test.py
+++ test.py
@@ -7,17 +7,22 @@ from furiosa.runtime.sync import create_runner
 from furiosa.quantizer import quantize, Calibrator, CalibrationMethod
 import onnx
 from utils.info import *
-from utils.preprocess import preproc
-from utils.postprocess import postproc
+from utils.preprocess import *
+from utils.postprocess import *
+import time
 
-image_path = './data/22_Picnic_Picnic_22_140.jpg'
+image_path = './data/22_Picnic_Picnic_22_10.jpg'
 
 
 with create_runner("yolov7_i8.onnx") as runner:
     image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image_tensor = preproc(image)
-    output = runner.run(image_tensor[0])
-    predictions = postproc(output, 0.65, 0.35)
 
-    import pdb; pdb.set_trace()
+    start = time.time()
+    for i in range(30):
+        image_tensor, preproc_params = preproc(image)
+        output = runner.run(image_tensor)
+        predictions = postproc(output, 0.65, 0.35)
+        predictions = predictions[0]
+        bboxed_img = draw_bbox(image, predictions, preproc_params)
+        cv2.imwrite('./output.png', bboxed_img)
+    print(str(time.time() - start))
